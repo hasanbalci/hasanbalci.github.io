bfefeee4b9a493385b31913f524ec34186d337d9
hasanbalci <balcihasan99@gmail.com>
2025-01-09 17:30:52 +0000

    Deploying to gh-pages from @ hasanbalci/hasanbalci.github.io@024fb2c52d4dabe12cd96ab4bbc909630f14fbf1 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -30,13 +30,6 @@ ninja.data = [{
           handler: () => {
             window.location.href = "/projects/";
           },
-        },{id: "nav-repositories",
-          title: "repositories",
-          description: "Edit the `_data/repositories.yml` and change the `github_users` and `github_repos` lists to include your own GitHub profile and repositories.",
-          section: "Navigation",
-          handler: () => {
-            window.location.href = "/repositories/";
-          },
         },{id: "nav-cv",
           title: "cv",
           description: "This is a description of the page. You can modify it in &#39;_pages/cv.md&#39;. You can also change or remove the top pdf download button.",
@@ -511,7 +504,7 @@ ninja.data = [{
         title: 'Google Scholar',
         section: 'Socials',
         handler: () => {
-          window.open("https://scholar.google.com/citations?user=qc6CJjYAAAAJ", "_blank");
+          window.open("https://scholar.google.com/citations?user=TXx-jbMAAAAJ", "_blank");
         },
       },{
         id: 'social-x',
