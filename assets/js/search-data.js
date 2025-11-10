0e9558e310df567d0f5900a8aa535ac2b2d62d4c
hasanbalci <balcihasan99@gmail.com>
2025-11-10 23:25:24 +0000

    Deploying to gh-pages from @ hasanbalci/hasanbalci.github.io@79f0bab180259e374e41a04bdf2ecb5bdab0dfa4 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -23,6 +23,13 @@ ninja.data = [{
           handler: () => {
             window.location.href = "/publications/";
           },
+        },{id: "nav-publications",
+          title: "publications",
+          description: "",
+          section: "Navigation",
+          handler: () => {
+            window.location.href = "/publications/";
+          },
         },{id: "nav-schedule",
           title: "schedule",
           description: "",
